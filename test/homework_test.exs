c5764c1f992450f87f58d892099e7e095d728256
taratimmerman <timmermantara@gmail.com>
2021-10-01 16:25:07 -0600

    Initial homework tests

--- test/homework_test.exs
+++ test/homework_test.exs
@@ -3,10 +3,26 @@ defmodule HomeworkTest do
   use Hound.Helpers
   use ExUnit.Case
 
+  # Screenshots on failure implementation
+  import Homework.ScreenshotsOnFailure, only: [test_sof: 2]
+
   # Start hound session and destroy when tests are run
   hound_session()
 
-  test "goes to google" do
-    navigate_to "http://google.com"
+  test_sof "landing page loads correctly" do
+    # Point browser to the-internet
+    navigate_to "https://the-internet.herokuapp.com/"
+
+    # Page title should be correct
+    assert page_title() =~ "The Internet"
+
+    # Example message should be visible
+    examplesTitle = find_element(:tag, "h2")
+    assert visible_text(examplesTitle) == "Available Examples"
+
+    # Should be at least 3 available examples
+    examples = find_all_elements(:tag, "li")
+    exampleAmount = Enum.count(examples)
+    assert exampleAmount >= 3
   end
 end
